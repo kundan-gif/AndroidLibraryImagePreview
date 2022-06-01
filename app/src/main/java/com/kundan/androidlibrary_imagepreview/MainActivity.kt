efcceca527b6defecc8de3ef96a5934ede86c997
kundan-gif <kundan211097@gmail.com>
2022-06-01 13:59:40 +0530

    Android Library Demo

---
+++ app/src/main/java/com/kundan/androidlibrary_imagepreview/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.kundan.androidlibrary_imagepreview
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kundan.androidlibrary_imagepreview.ui.theme.AndroidLibraryImagePreviewTheme
+import com.kundan.image_preview.ImagePreview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidLibraryImagePreviewTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                      //ImagePreview(image = )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AndroidLibraryImagePreviewTheme {
+        Greeting("Android")
+    }
+}
